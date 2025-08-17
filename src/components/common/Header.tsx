7dd5008823970c680b6984cc623b9fd368e747b4
Mayhem <sujeetsupremacy@gmail.com>
2025-08-17 11:22:12 +0000

    you should move it to little right.

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -38,7 +38,7 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 max-w-screen-2xl items-center justify-between">
         <div className="flex items-center gap-10">
-          <Link href="/" className="flex items-center space-x-2">
+          <Link href="/" className="flex items-center space-x-2 ml-4">
             <span className="font-bold">Movora</span>
           </Link>
           <nav className="hidden gap-6 md:flex">
