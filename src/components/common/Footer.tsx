c11406f37bc240167609fa4b84fba55ddbb75030
Mayhem <sujeetsupremacy@gmail.com>
2025-08-17 21:15:53 +0000

    write it in your way, little shorter

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -8,8 +8,8 @@ export function Footer() {
         <p className="text-sm mt-2">
           This project uses the TMDB API but is not endorsed or certified by TMDB.
         </p>
-        <p className="text-xs mt-4 max-w-4xl mx-auto">
-            DMCA Disclaimer: Movora is a content aggregator that scrapes publicly available sources from the internet. We do not host, upload, or store any copyrighted content on our servers. All videos, images, and links displayed on this site are gathered automatically from third-party websites. If you are a copyright owner and believe that any content found through Movora violates your rights, please be aware that we do not control or host any of the content. Any copyright infringement claims should be directed to the respective third-party sites where the content is hosted. For any DMCA takedown requests, please contact the original content provider or hosting website.
+        <p className="text-xs mt-4 max-w-3xl mx-auto">
+          Movora is a search engine for streaming links and does not host any files on its servers. All content is provided by non-affiliated third parties. Copyright infringement claims should be directed to the content hosting sites.
         </p>
       </div>
     </footer>
