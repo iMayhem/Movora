e6ee9d7b37742a5db9647779462318d57608f775
Mayhem <sujeetsupremacy@gmail.com>
2025-08-17 11:45:41 +0000

    decrease the length og searchbox

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -60,7 +60,7 @@ export function Header() {
           </nav>
         </div>
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <form onSubmit={handleSearch} className="relative w-full max-w-sm">
+          <form onSubmit={handleSearch} className="relative w-full max-w-xs">
             <Input
               type="search"
               placeholder="Search movies & shows..."
