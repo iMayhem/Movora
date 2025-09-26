b251f6702272006e410fbdf6036e01ffaa15a209
iMayhem <sujeetunbeatable@gmail.com>
2025-09-26 18:23:39 +0530

    Add generated plugin registrant file and update widget tests for Movora ap

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:movora/main.dart';
 
@@ -5,8 +6,16 @@ void main() {
   testWidgets('Movora app smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MovoraApp());
+    await tester.pumpAndSettle();
 
-    // Verify that the app title is displayed
-    expect(find.text('Movora'), findsOneWidget);
+    // Verify that the Hollywood title is displayed (main screen title)
+    expect(find.text('Hollywood'), findsAtLeastNWidgets(1));
+
+    // Open the drawer to check for Movora branding
+    await tester.tap(find.byIcon(Icons.menu));
+    await tester.pumpAndSettle();
+
+    // Verify that the Movora title is displayed in the drawer
+    expect(find.text('MOVORA'), findsOneWidget);
   });
 }
