15d8693db00a4e1274a3d4de600a02e6ecb4fa07
iMayhem <67825237+iMayhem@users.noreply.github.com>
2025-11-21 14:36:43 +0530

    Delete lib directory

--- lib/config/runtime_flags.dart
+++
@@ -1,8 +0,0 @@
-class RuntimeFlags {
-  static bool _lowDataEnabled = false;
-
-  static bool get lowDataEnabled => _lowDataEnabled;
-  static void setLowData(bool enabled) {
-    _lowDataEnabled = enabled;
-  }
-}
